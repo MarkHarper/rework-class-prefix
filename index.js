f28162b998631381366ba279d024a1e9bac3e120
Jan Vlnas <git@jan.vlnas.cz>
2014-12-17 17:07:41 +0100

    Implement except option

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 'use strict';
 
-module.exports = function classPrefix(prefix) {
+function classMatchesTest(klass, test) {
+  if(!test) {
+    return false;
+  }
+
+  if(test instanceof RegExp) {
+    return test.exec(klass);
+  }
+  return klass === test;
+}
+
+function isClassSelector(selector) {
+  return selector.indexOf('.') === 0;
+}
+
+module.exports = function classPrefix(prefix, options) {
+  options = options || {};
+
+  function isIgnoredClass(klass) {
+    return classMatchesTest(klass, options.except);
+  }
+
   return function classPrefix(styling) {
     var walk = require('rework-walk');
     walk(styling, function(rule, node) {
-      if (!rule.selectors) return rule;
+      if (!rule.selectors){
+        return rule;
+      }
 
       rule.selectors = rule.selectors.map(function(selector) {
-        if (selector.indexOf('.') === 0) {
-          return selector.split('.').join('.' + prefix);
-        } else {
+        if (!isClassSelector(selector)) {
           return selector;
         }
+
+        var classes = selector.split('.');
+
+        return classes.map(function(klass){
+          if(isIgnoredClass(klass) || klass.trim().length === 0) {
+            return klass;
+          }
+          return prefix + klass;
+        }).join('.');
       });
     });
   };
